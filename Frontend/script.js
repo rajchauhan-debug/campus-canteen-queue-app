76969d2e1f670094006b74c8d8e3db2709f6b235
Raj Chauhan <rc9369586134@gmail.com>
2026-09-14 14:57:56 +0530

    Update ALB endpoint

--- Frontend/script.js
+++ Frontend/script.js
@@ -220,7 +220,7 @@ async function placeOrder() {
         // Send order to backend
         const response =
             await fetch(
-                "http://campus-canteen-alb-1373901949.ap-south-1.elb.amazonaws.com/api/orders",
+                "http://campus-canteen-alb-271072627.ap-south-1.elb.amazonaws.com",
                 {
 
                     method: "POST",
